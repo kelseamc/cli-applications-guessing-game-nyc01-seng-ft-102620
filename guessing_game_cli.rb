63cfbfef898c085bdc5400a30604364ce3aa1b92
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-24 14:14:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,2 +1,11 @@
 def run_guessing_game
+  x = rand(1..6)
+  puts "Please guess a number between 1 and 6"
+  input = gets.chomp
+  if input == x 
+    puts "You guessed the correct number!"
+  elsif input != x 
+    puts "Sorry! The computer guessed #{x}"
+  elsif input == "exit"
+    puts "Goodbye!"
 end
